6f9df46e4e98583f389d6dd2732b32ba7b2e5c5b
Andres Valdez <andresevaldez@gmail.com>
2022-04-21 00:36:18 -0500

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import BusinessList from "../BusinessList/BusinessList";
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
